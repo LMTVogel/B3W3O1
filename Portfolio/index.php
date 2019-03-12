375eaf8ad7eacbabd371ff297d406fb3b1e31c1a
Luuk Vogel <99051050@mydavinci.nl>
2019-03-12 14:30:58 +0100

    UPDATE: Navbar added.

---
+++ Portfolio/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Home || Luuk Vogel</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+	<nav>
+		<?php 
+			include "include/navbar.php"
+		 ?>
+	</nav>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
